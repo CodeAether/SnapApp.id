4be29afeb6900b3a29bda032172c53c232c9ef31
OmDutaa <85216552+DutaKey@users.noreply.github.com>
2023-07-16 10:08:56 +0700

    update: token function

--- router/router.js
+++ router/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express.Router();
 const axios = require("axios");
+const privateKey = "codeaether";
+const CryptoJS = require("crypto-js");
 
 app.get("/", async (req, res) => {
 	if (req.language === "id") {
@@ -47,13 +49,26 @@ app.get("/download/en", async (req, res) => {
 });
 
 app.post("/api", async (req, res) => {
+	const timestamp = Math.floor(Date.now() / 1000);
 	if (!req.body.url) return res.status(400).json(["Url Missing"]);
+	if (!req.body.type) return res.status(400).json(["Invalid Type"]);
+	if (!req.body.token) return res.status(400).json(["Invalid Token"]);
 	try {
+		token = req.body.token;
 		type = req.body.type;
-		url = req.body.url || "";
+		url = req.body.url;
+		const bytes = CryptoJS.AES.decrypt(token, privateKey);
+		const unencryptToken = bytes.toString(CryptoJS.enc.Utf8);
+		const urlFromToken = unencryptToken.split("||+||")[0] || "";
+		const timestampFromToken = unencryptToken.split("||+||")[1] || "";
+		const differentTime = timestamp - timestampFromToken;
+		if (urlFromToken != url)
+			return res.status(400).json(["something is wrong with the token"]);
+		if (differentTime > 30) return res.status(400).json(["token are expired"]);
 		const data = await axios.post(`http://localhost:3000/api`, {
 			url,
 			type,
+			token,
 		});
 		res.json(data.data);
 	} catch (e) {
